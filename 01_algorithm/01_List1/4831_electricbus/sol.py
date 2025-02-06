48e26d6005259f09c6e94265f9be68d097044bc7
skdbsqls <skdbsqls98@naver.com>
2025-02-06 21:37:11 +0900

    4831_전기 버스

--- 01_algorithm/01_List1/4831_electricbus/sol.py
+++ 01_algorithm/01_List1/4831_electricbus/sol.py
@@ -3,7 +3,7 @@ import sys
 sys.stdin = open('sample_input.txt')
 
 T = int(input())
-for test_case in range(1):
+for test_case in range(1, T + 1):
     [K, N, M] = list(map(int, input().split()))
     charging_stations = list(map(int, input().split()))
 
@@ -11,17 +11,15 @@ for test_case in range(1):
     current = 0  # 버스의 현재 위치
 
     # 도착할 때까지 반복
-
-    for i in range(current + K, current, -1):
-        if i in charging_stations:
-            current = i
-            count += 1
-            break
+    while current + K < N:  # N번 정류장까지 이동
+        for location in range(current + K, current, -1):  # 현재 위치에서 가장 멀리 갈 수 있는 충전소 찾기
+            if location in charging_stations:
+                current = location  # 현재 위치 수정
+                count += 1  # 충전 횟수 + 1
+                break
+        # 충전소가 없어서 도착하지 못 하는 경우
         else:
             count = 0
             break
-
-        # if count == 0:
-        #     break
 
     print(f'#{test_case} {count}')
