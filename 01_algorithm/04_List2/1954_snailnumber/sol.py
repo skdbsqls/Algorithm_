2b91da33a42a2d4e53fbf8152a7a8d1f300fb6fc
skdbsqls <skdbsqls98@naver.com>
2025-02-11 14:34:55 +0900

    1954_달팽이숫자2

--- 01_algorithm/04_List2/1954_snailnumber/sol.py
+++ 01_algorithm/04_List2/1954_snailnumber/sol.py
@@ -4,6 +4,7 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 
+# 풀이 1)
 for tc in range(1, T + 1):
     N = int(input())
     snail = [[0] * N for _ in range(N)]
@@ -35,3 +36,38 @@ for tc in range(1, T + 1):
     print(f'#{tc}')
     for s in snail:
         print(f'{" ".join(map(str, s))}')
+
+
+# 풀이 2)
+N = 4
+# 달팽이가 될 2차원 배열 선언
+result = [[0] * N for _ in range(N)]
+
+# 우하좌상 순서대로 델타 선언
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+# 현재 방향
+dir_idx = 0
+
+# 현재 위치
+now_i = 0
+now_j = 0
+
+# 달팽이 숫자 만들기
+for next_num in range(1, N * N + 1):
+    # 현재 위치에 숫자 넣기
+    result[now_i][now_j] = next_num
+
+    # 현재 진행방향으로 더 진행할 수 있는지 판단
+    # 인덱스가 유효한지, 다음 위치에 0이 있는지를 기준으로
+    if not (0 <= now_i + di[dir_idx] < N and 0 <= now_j + dj[dir_idx] < N and result[now_i + di[dir_idx]][now_j + dj[dir_idx]] == 0):
+        # 만약 그렇지 않으면, 방향 전환
+        dir_idx = (dir_idx + 1) % 4
+
+    # 다음 위치 지정
+    now_i += di[dir_idx]
+    now_j += dj[dir_idx]
+
+for row in result:
+    print(f'{" ".join(map(str, row))}')
