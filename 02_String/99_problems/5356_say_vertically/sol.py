a646bb94a13cd4846fe2468eb1d4905ea4e07159
skdbsqls <skdbsqls98@naver.com>
2025-02-12 14:16:59 +0900

    5356_의석이의세로로말해요2

--- 02_String/99_problems/5356_say_vertically/sol.py
+++ 02_String/99_problems/5356_say_vertically/sol.py
@@ -2,6 +2,7 @@ import sys
 
 sys.stdin = open('sample_input.txt')
 
+# 풀이 1
 T = int(input())
 for tc in range(1, T + 1):
     board = [list(input()) for _ in range(5)]
@@ -28,3 +29,27 @@ for tc in range(1, T + 1):
                 result += arr[i][j]
 
     print(f'#{tc} {result}')
+
+# 풀이 2
+T = int(input())
+for tc in range(1, T + 1):
+    board = [list(input()) for _ in range(5)]
+
+    # 가장 긴 행의 길이 구하기
+    max_row = 0
+    for row in board:
+        max_row = max(max_row, len(row))
+
+    letters = []
+
+    # 세로로 순회
+    for i in range(max_row):  # 가장 긴 행의 길이만큼 반복
+        for j in range(5):  # 안쪽은 5번만 반복
+            # j번째 줄이 현재 확인하고 있는 i보다 길다는 것을 확인
+            if len(board[j]) <= i:
+                # 작거나 같으면 나머지는 실행 안 함
+                continue
+            # 글자를 추가해준다.
+            letters.append(board[j][i])  # 먼저 바뀌는 쪽이 앞에 옴
+
+    print(f'#{tc} {"".join(letters)}')
