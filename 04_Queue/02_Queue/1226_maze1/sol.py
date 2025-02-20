ae39e9aabef72f0336ff103318e49f123542d1de
skdbsqls <skdbsqls98@naver.com>
2025-02-20 12:16:18 +0900

    1226_미로1

---
+++ 04_Queue/02_Queue/1226_maze1/sol.py
@@ -0,0 +1,40 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+for _ in range(10):
+    tc = int(input())
+    maze = [list(map(int, input())) for _ in range(16)]
+
+    success = 0  # 결과
+
+    # 탐색하기
+    deltas = [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+    ]
+
+    my_queue = [[1, 1]]  # 시작점은 (1, 1)
+    maze[1][1] = 1  # 시작점 방문 처리
+
+    # 더이상 방문할 곳이 없을 때까지 반복
+    while my_queue:
+        ti, tj = my_queue.pop(0)  # 큐에서 꺼내기(현위치)
+
+        # 델타 탐색하기
+        for delta in deltas:
+            ni, nj = ti + delta[0], tj + delta[1]  # 다음 위치
+
+            # 다음 위치가 끝점이면 성공
+            if maze[ni][nj] == 3:
+                success = 1
+                break
+
+            # 유효한 인덱스이면서, 길(0)이면 go
+            if 0 <= ni < 16 and 0 <= nj < 16 and maze[ni][nj] == 0:
+                my_queue.append([ni, nj])  # 큐에 넣고
+                maze[ni][nj] = 1  # 방문 표시
+
+    print(f'#{tc} {success}')
