a668547bfa05c5bea154a074f534a61814e51747
skdbsqls <skdbsqls98@naver.com>
2025-04-08 22:31:40 +0900

    2961_도영이가만든맛있는음식

--- BAEKJOON/April_Week1/2961.py
+++ BAEKJOON/April_Week1/2961.py
@@ -1,20 +1,17 @@
 N = int(input())  # N개의 재료
-SB = [list(map(int, input().split())) for _ in range(N)]
+SB = [list(map(int, input().split())) for _ in range(N)]  # 신맛과 쓴맛 배열
+min_v = 1e9  # 신맛과 쓴맛의 차이 최소값
 
-# 신맛의 곱 계산하기
-def cal_S(cnt, total):
+def recur(cnt, sour, bitter):
+    global min_v
     if cnt == N:
-        print(total)
+        min_v = min(min_v, abs(sour - bitter))
         return
 
-    cal_S(cnt + 1, total * SB[cnt][0])  # 포함 하는 경우
-    cal_S(cnt + 1, total)  # 포함 안 하는 경우
+    recur(cnt + 1, sour * SB[cnt][0], bitter + SB[cnt][1])  # 재료를 사용한 경우
+    recur(cnt + 1, sour, bitter)  # 사용하지 않은 경우
 
-# 쓴맛의 합 계산하기
-def cal_B(cnt, total):
-    if cnt == N:
-        print(total)
-        return
+for i in range(N):
+    recur(i + 1, SB[i][0], SB[i][1])  # 적어도 하나 이상의 재료를 사용해야 함
 
-    cal_B(cnt + 1, total + SB[cnt][1])
-    cal_B(cnt + 1, total)
+print(min_v)
